f00d48a530cddc78a29c559ca2aa3ed507186796
MihailNMihaylov <m.mihaylov88@abv.bg>
2018-12-10 13:29:24 +0200

    Created asp.net core MVC project

---
+++ src/Web/FoodBank.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodBank.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
