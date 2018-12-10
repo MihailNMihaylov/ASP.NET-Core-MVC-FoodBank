ed700fae5dfd7943db316b36852ee9c3eb7e6d07
MihailNMihaylov <m.mihaylov88@abv.bg>
2018-12-10 13:47:56 +0200

    scaffolded Identity and added custom user class

--- src/Web/FoodBank.Web/Startup.cs
+++ src/Web/FoodBank.Web/Startup.cs
@@ -10,9 +10,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using FoodBank.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FoodBank.Web.Models;
+using FoodBank.Web.Areas.Identity.Data;
 
 namespace FoodBank.Web
 {
@@ -35,12 +36,13 @@ namespace FoodBank.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
+
+            services.AddDbContext<FoodBankContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                    this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<FoodBankUser>()
+                .AddEntityFrameworkStores<FoodBankContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
